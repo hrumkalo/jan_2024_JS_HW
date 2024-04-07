9fdaef44215739eff7f5dd77699655a02ebd8e14
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-04-08 00:25:35 +0300

    HomeWork_Les5

--- HomeWork_JS_4/JS_4.js
+++ HomeWork_JS_4/JS_4.js
@@ -4,24 +4,29 @@ let currencyForExchange = prompt('If you have 1 000 UAH, you can exchange USD or
 let weHave = [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}];
 
 if (currencyForExchange === 'USD' || currencyForExchange === 'EURO') {
-    document.write('Let`s exchange ');
+    document.write('Let`s exchange your money');
 } else {
     document.write('Please choose, and write "USD" or "EURO"');
 }
 function exchange(sumUAH, currencyValues, exchangeCurrency) {
     let sumUA = 1000;
-    for (const item of currencyValues){
+    for (const item of currencyValues) {
         if (item.currency === exchangeCurrency) {
-            let exchangedSum = sumUAH / item.value;
-            document.write('Let`s exchange');
-            return exchangedSum;
+            if (exchangeCurrency === 'USD') {
+                let exchangedSum = sumUAH / item.value;
+                console.log('Let`s exchange to USD');
+                return exchangedSum;
+            } else if (exchangeCurrency === 'EURO') {
+                let exchangedSum = sumUAH / item.value;
+                console.log('Let`s exchange to EURO');
+                return exchangedSum;
+            }
         }
     }
-    // Якщо валюта для обміну не знайдена
-    document.write('Currency not available for exchange. Please choose, and write "USD" or "EURO"');
+    // Повторне введення валюти, якщо валюта для обміну не знайдена
+    let newCurrency = prompt('Currency not available for exchange. Please choose, and write "USD" or "EURO"');
+    return exchange(sumUAH, currencyValues, newCurrency);
 }
-
 // Виклик функції та виведення результату
-let currencyForExchange = prompt('If you have 1 000 UAH, you can exchange USD or EURO. Which currency do you want exchange?');
 let result = exchange(1000, weHave, currencyForExchange);
-document.write(result);
+console.log(result);
