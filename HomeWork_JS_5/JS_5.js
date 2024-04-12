aab505c2222a99d199bc0587f2544474f52267d2
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-04-13 00:43:15 +0300

    HomeWork_Les5_5/2

--- HomeWork_JS_5/JS_5.js
+++ HomeWork_JS_5/JS_5.js
@@ -4,3 +4,61 @@
 // console.log(findS (10, 2));
 
 //------------------------------------------------------5/2------------------------------------------------------
+
+// function circleR(radius){
+//     return 3.14 * radius * radius;
+// }
+// document.write(circleR(25));
+
+const circleR = (radius = 10) => { return 3.14 * radius * radius; }
+document.write(circleR(radius));
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------4/13------------------------------------------------------
+
+// let currencyForExchange = prompt('If you have 1 000 UAH, you can exchange USD or EURO. Which currency do you want exchange?');
+// let weHave = [{currency: 'USD', value: 40}, {currency: 'EURO', value: 42}];
+//
+// if (currencyForExchange === 'USD' || currencyForExchange === 'EURO') {
+//     document.write('Let`s exchange your money');
+// } else {
+//     document.write('Please choose, and write "USD" or "EURO"');
+// }
+// function exchange(sumUAH, currencyValues, exchangeCurrency) {
+//     let sumUA = 1000;
+//     for (const item of currencyValues) {
+//         if (item.currency === exchangeCurrency) {
+//             if (exchangeCurrency === 'USD') {
+//                 let exchangedSum = sumUAH / item.value;
+//                 console.log('Let`s exchange to USD');
+//                 return exchangedSum;
+//             } else if (exchangeCurrency === 'EURO') {
+//                 let exchangedSum = sumUAH / item.value;
+//                 console.log('Let`s exchange to EURO');
+//                 return exchangedSum;
+//             }
+//         }
+//     }
+//     // Повторне введення валюти, якщо валюта для обміну не знайдена
+//     let newCurrency = prompt('Currency not available for exchange. Please choose, and write "USD" or "EURO"');
+//     return exchange(sumUAH, currencyValues, newCurrency);
+// }
+//
+// // Виклик функції та виведення результату
+// let currencyForExchangeUSD = 'USD'; // Валюта для обміну
+// let resultUSD = exchange(1000, weHave, currencyForExchangeUSD);
+// console.log(resultUSD);
+//
+// // Виклик функції та виведення результату для обміну на EUR
+// let currencyForExchangeEURO = 'EURO'; // Валюта для обміну
+// let resultEURO = exchange(1000, weHave, currencyForExchangeEURO);
+// console.log(resultEURO);
