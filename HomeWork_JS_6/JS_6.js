5a6f0cfe6b6ec3d11d2b7e4e022d5e742d40d769
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-04-28 12:27:05 +0300

    HomeWork_Les6_1.1

--- HomeWork_JS_6/JS_6.js
+++ HomeWork_JS_6/JS_6.js
@@ -8,8 +8,9 @@ let strings = [
     {text3: 'javascript is cool'},
 ]
 console.log(strings.length);
-
-
+console.log(strings[0].text1.length);
+console.log(strings[1].text2.length);
+console.log(strings[2].text3.length);
 
 
 // Перевести до великого регістру наступні стрінгові значення: 'hello world', 'lorem ipsum', 'javascript is cool'
