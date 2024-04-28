ad1de85f34b9c46f37f427f8cc52f2840ed9ffa6
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-04-28 12:46:22 +0300

    HomeWork_Les6_1.3

--- HomeWork_JS_6/JS_6.js
+++ HomeWork_JS_6/JS_6.js
@@ -2,20 +2,34 @@
 
 // Знайти та вивести довижину настипних стрінгових значень: 'hello world', 'lorem ipsum', 'javascript is cool'
 
-let strings = [
-    {text1: 'hello world'},
-    {text2: 'lorem ipsum'},
-    {text3: 'javascript is cool'},
-]
-console.log(strings.length);
-console.log(strings[0].text1.length);
-console.log(strings[1].text2.length);
-console.log(strings[2].text3.length);
-
+// let strings = [
+//     {text1: 'hello world'},
+//     {text2: 'lorem ipsum'},
+//     {text3: 'javascript is cool'},
+// ]
+// console.log(strings.length);
+// console.log(strings[0].text1.length);
+// console.log(strings[1].text2.length);
+// console.log(strings[2].text3.length);
 
 // Перевести до великого регістру наступні стрінгові значення: 'hello world', 'lorem ipsum', 'javascript is cool'
 
+// let toUpperCase = strings[0].text1.toUpperCase();
+// console.log(toUpperCase);
+// let toUpperCase = strings[1].text2.toUpperCase();
+// console.log(toUpperCase);
+// let toUpperCase = strings[2].text3.toUpperCase();
+// console.log(toUpperCase);
+
 // Перевести до нижнього регістру настипні стрінгові значення: 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// let strings = [
+//     {text1: 'HELLO WORLD'},
+//     {text2: 'LOREM IPSUM'},
+//     {text3: 'JAVASCRIPT IS COOL'},
+// ]
+// let toLowerCase = strings[0].text1.toLowerCase();
+// console.log(toLowerCase);
 
 // Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
